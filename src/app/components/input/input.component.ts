4b74271fbee8c313f50956c81fd1df7a027d9aad
Rogerio Martins Campos Martins Campos <zzmelzz@hotmail.com>
2023-03-02 17:33:53 -0300

    0203

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  valorItem!: string;
+
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void { }
+
+  adicionarItem(){
+    this.listaService.adicionarItemnaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = "";
+  }
+}
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges {
+
+  @Input() item!: Item;
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnChanges(): void {
+    console.log('OnInit');
+  }
+
+  ngOnInit(): void { 
+    console.log('Onchanges');
+  }
+
+}
